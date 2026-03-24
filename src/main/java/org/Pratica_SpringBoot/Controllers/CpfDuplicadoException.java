4671d84374a8f30d816471b11783e47a5655f919
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2026-03-24 16:46:06 -0300

    fix: Remove arquivo CpfDuplicadoException.java

--- src/main/java/org/Pratica_SpringBoot/Controllers/CpfDuplicadoException.java
+++
@@ -1,8 +0,0 @@
-package org.Pratica_SpringBoot.Controllers;
-
-public class CpfDuplicadoException extends RuntimeException {
-
-    public CpfDuplicadoException(String message) {
-        super(message);
-    }
-}
